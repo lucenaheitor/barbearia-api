b5fe8637bc71e6752ed489ae91283515a0e78795
lucenaheitor <lucenaheitor@gmail.com>
2024-02-04 21:02:39 -0300

    instalçação e criação do usuario e spring security

---
+++ src/main/java/lucenaheitor/io/barbearia/domain/usuario/Usuario.java
@@ -0,0 +1,67 @@
+package lucenaheitor.io.barbearia.domain.usuario;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario  implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+    private String role;
+
+    public  Usuario(String login, String password){
+        this.login = login;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}

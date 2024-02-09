b41426b307e7cf1448c93be48599e77e0adc8c5b
lucenaheitor <lucenaheitor@gmail.com>
2024-02-09 09:54:27 -0300

    implementado regra de negocio q verifica se o  login ja existe

--- src/main/java/lucenaheitor/io/barbearia/controler/LoginController.java
+++ src/main/java/lucenaheitor/io/barbearia/controler/LoginController.java
@@ -5,6 +5,7 @@ import lucenaheitor.io.barbearia.domain.usuario.AuthenticationDTO;
 import lucenaheitor.io.barbearia.domain.usuario.RegisterDTO;
 import lucenaheitor.io.barbearia.domain.usuario.Usuario;
 import lucenaheitor.io.barbearia.domain.usuario.UsuarioRepository;
+import lucenaheitor.io.barbearia.infra.exception.ValidationExeception;
 import lucenaheitor.io.barbearia.infra.security.DataTokenJWT;
 import lucenaheitor.io.barbearia.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,36 +32,24 @@ public class LoginController {
     @Autowired
     private TokenService tokenService;
 
+
+
     @PostMapping("login")
     public  ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
+
+
        var userNamePaaword =  new UsernamePasswordAuthenticationToken(data.login(), data.password());
        var auth = this.authenticationManager.authenticate(userNamePaaword);
        var token = tokenService.generateToken((Usuario) auth.getPrincipal());
-
        return  ResponseEntity.ok(new DataTokenJWT(token));
-//        try {
-//
-//            System.out.println("Login:"+data.login());
-//            System.out.println("Password:"+data.password());
-//
-//            var authenticationToken = new UsernamePasswordAuthenticationToken(data.login(), data.password());
-//
-//            var auth = authenticationManager.authenticate(authenticationToken);
-//            System.out.println("Authentication Object:"+auth);
-//
-//            var tokenJWT = tokenService.generateToken((Usuario)  auth.getPrincipal());
-//
-//            return ResponseEntity.ok(new DataTokenJWT(tokenJWT));
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return ResponseEntity.badRequest().body(e.getMessage());
-//        }
     }
 
 
     @PostMapping("/register")
     public  ResponseEntity register(@RequestBody @Valid RegisterDTO data){
-        if (this.repository.findByLogin(data.login()) != null) return  ResponseEntity.badRequest().build();
+        if (this.repository.findByLogin(data.login()) != null){
+            throw  new ValidationExeception("login ja existe");
+        }
 
         String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
         Usuario newUser = new Usuario(data.login(),  encryptedPassword, data.role());

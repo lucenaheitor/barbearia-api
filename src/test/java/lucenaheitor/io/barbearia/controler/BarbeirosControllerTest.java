f3b380578c9fbc31eb111732fd91161de2272444
Heitor <lucenaheitor@gmail.com>
2024-11-11 16:07:03 -0300

    test unitario  register barbeiro controller test feito com sucesso

--- src/test/java/lucenaheitor/io/barbearia/controler/BarbeirosControllerTest.java
+++ src/test/java/lucenaheitor/io/barbearia/controler/BarbeirosControllerTest.java
@@ -1,19 +1,17 @@
 package lucenaheitor.io.barbearia.controler;
 
-import lucenaheitor.io.barbearia.domain.agenda.DetalhamentoCorteDeCabelo;
-import lucenaheitor.io.barbearia.domain.barbeiros.*;
-import lucenaheitor.io.barbearia.controler.BarbeirosController;
+import lucenaheitor.io.barbearia.domain.barbeiros.Barbeiro;
+import lucenaheitor.io.barbearia.domain.barbeiros.BarbeiroRepository;
+import lucenaheitor.io.barbearia.domain.barbeiros.CadastroBarbeiroDTO;
+import lucenaheitor.io.barbearia.domain.barbeiros.Especialidade;
 import lucenaheitor.io.barbearia.infra.service.BarbeiroService;
-import lucenaheitor.io.barbearia.infra.security.TokenService;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -22,87 +20,86 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-@WebMvcTest(controllers = BarbeirosController.class)
-@AutoConfigureMockMvc(addFilters = false) // Desativa filtros de segurança
-@ExtendWith(MockitoExtension.class)
-@AutoConfigureJsonTesters
+
+@AutoConfigureMockMvc(addFilters = false) // Desativa os filtros de segurança
+@SpringBootTest
 class BarbeirosControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private JacksonTester<CadastroBarbeiroDTO> cadastroBarbeiroDTOJson;
-
-    @Autowired
-    private JacksonTester<DetailsBarbeiros> detailsBarbeirosJson;
-
     @MockBean
     private BarbeiroRepository barbeiroRepository;
 
     @MockBean
-    private BarbeiroService barbeiroService;
+    private BarbeiroService service;
 
-    @MockBean
-    private TokenService tokenService; // Mock do TokenService
+     @Test
+     @WithMockUser(username = "testuser", roles = {"ADMIN"})
+     void register() throws Exception {
+        CadastroBarbeiroDTO dto = new CadastroBarbeiroDTO(
+                "Test",
+                "test@example.com",
+                "123.456.789-00",
+                "1234567890",
+                Especialidade.CORTE_BARBA );
+        when(service.register(any(CadastroBarbeiroDTO.class)))
+                .thenReturn(dto);
+       var  response =  mockMvc.perform(post("/barbeiros")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"nome\":\"Test\", \"email\":\"test@example.com\", \"cpf\":\"123.456.789-00\", \"telefone\":\"1234567890\", \"especialidade\":\"CORTE\"}"))
+                        .andReturn().getResponse();
 
-    @Test
-    @WithMockUser(username = "admin", roles = {"ADMIN"})
-    public void testCadastrar() throws Exception {
-        var cadastroBarbeiroDTO = new CadastroBarbeiroDTO(
-                "teste",
-                "barbeiro@barbearia.com",
-                "61999999999",
-                "(11)123456789",
-                Especialidade.CORTE_BARBA
-        );
+                Assertions.assertEquals(201, response.getStatus());
+        }
 
-        when(barbeiroRepository.save(any())).thenReturn(new Barbeiro(cadastroBarbeiroDTO));
+//    @Test
+//    @WithMockUser(username = "testuser", roles = {"ADMIN"})
+//    void list() throws Exception {
+//        List<Barbeiro> barbeiros = new ArrayList<>();
+//        barbeiros.add(new Barbeiro());
+//        when(service.getBarbeiro(any())).thenReturn(barbeiros);
+//
+//        mockMvc.perform(get("/barbeiros")
+//                        .param("page", "0")
+//                        .param("size", "5"))
+//                .andExpect(status().isOk());
+//    }
 
-        var response = mockMvc.perform(post("/barbeiros")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(cadastroBarbeiroDTOJson.write(cadastroBarbeiroDTO).getJson()))
-                .andExpect(status().isOk())
-                .andReturn().getResponse();
-
-        var details = new DetailsBarbeiros(
-                null,
-                cadastroBarbeiroDTO.nome(),
-                cadastroBarbeiroDTO.email(),
-                cadastroBarbeiroDTO.cpf(),
-                cadastroBarbeiroDTO.telefone(),
-                cadastroBarbeiroDTO.especialidade()
-        );
-
-        var jsonEsperado = detailsBarbeirosJson.write(details).getJson();
-        assertEquals(jsonEsperado, response.getContentAsString());
-    }
-
-    @Test
-    @WithMockUser(username = "admin", roles = {"ADMIN"})
-    public void testDeletar() throws Exception {
-        doNothing().when(barbeiroRepository).deleteById(anyLong());
-
-        mockMvc.perform(delete("/barbeiros/1"))
-                .andExpect(status().isNoContent());
-    }
-
-    @Test
-    @WithMockUser(username = "admin", roles = {"ADMIN"})
-    public void testListar() throws Exception {
-        List<Barbeiro> barbeiros = new ArrayList<>();
-        barbeiros.add(new Barbeiro());
-        when(barbeiroRepository.findAll()).thenReturn(barbeiros);
-
-        mockMvc.perform(get("/barbeiros"))
-                .andExpect(status().isOk());
-    }
+//    @Test
+//    @WithMockUser(username = "testuser", roles = {"ADMIN"})
+//    void detail() throws Exception {
+//        Barbeiro barbeiro = new Barbeiro();
+//        when(service.datails(anyLong())).thenReturn(barbeiro);
+//
+//        mockMvc.perform(get("/barbeiros/1"))
+//                .andExpect(status().isOk());
+//    }
+//
+//    @Test
+//    @WithMockUser(username = "testuser", roles = {"ADMIN"})
+//    void update() throws Exception {
+//        Barbeiro barbeiro = new Barbeiro();
+//        when(service.update(any())).thenReturn(barbeiro);
+//
+//        mockMvc.perform(put("/barbeiros")
+//                        .contentType("application/json")
+//                        .content("{\"nome\":\"Test\", \"email\":\"test@example.com\", \"cpf\":\"123.456.789-00\", \"telefone\":\"1234567890\", \"especialidade\":\"CORTE\"}"))
+//                .andExpect(status().isOk());
+//    }
+//
+//    @Test
+//    @WithMockUser(username = "testuser", roles = {"ADMIN"})
+//    void delete() throws Exception {
+//        doNothing().when(service).delete(anyLong());
+//
+//        mockMvc.perform(delete())
+//                .andExpect(status().isNoContent());
+//    }
 }
